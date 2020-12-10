c898ca035417136166d47ba3259601690b82aa86
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2020-12-10 17:38:25 +0200

    Posts now show all details

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,33 @@
     <ul>
         <li>Title: {{ $post->title }}</li>
         <li>Content: {{ $post->content }}</li>
-        <li><a href="{{ route('users.show', ['id' => $post->user->id]) }}">Author: {{ $post->user->name}}</a></li>
+        <li>Author: <a href="{{ route('users.show', ['id' => $post->user->id]) }}">{{ $post->user->name}}</a></li>
+        <li>Tags:</li>
+            <ul>
+            @foreach ($post->tags as $tag)
+                <li>{{ $tag->name}}</li>
+            @endforeach
+            </ul>
+
+        <li>Images:</li>
+            <ul>
+            @foreach ($post->images as $image)
+                <li>{{ $image->url}}</li>
+            @endforeach
+            </ul>
+
+        <li>Ingredients:</li>
+            <ul>
+            @foreach ($post->ingredients as $ingredient)
+                <li>{{ $ingredient->name}} {{ $ingredient->amount}}{{ $ingredient->measurement}}</li>
+            @endforeach
+            </ul>
+
+        <li>Comments:</li>
+            <ul>
+            @foreach ($post->comments as $comment)
+                <li><a href="{{ route('users.show', ['id' => $comment->user->id]) }}">{{ $comment->user->name}}</a> <br> {{ $comment->content}}</li>
+            @endforeach
+            </ul>
     </ul>
 @endsection
