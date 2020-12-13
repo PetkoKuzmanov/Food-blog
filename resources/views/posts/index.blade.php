26bd07f417fbf2dc69f705414058879c635a9e6a
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2020-12-13 14:41:01 +0200

    Added pagination of the posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,15 +3,21 @@
 @section('title', "Posts")
 
 @section('content')
+<div class="container mt-1">
     <p>The posts of the website:</p>
 
     @if (session('message'))
-        <p><b>{{ session('message')}}</b></p>
+    <p><b>{{ session('message')}}</b></p>
     @endif
-    
+
     <ul>
         @foreach ($posts as $post)
-            <li><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title}}</a></li>
+        <li><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title}}</a></li>
         @endforeach
     </ul>
+
+    <div class="d-flex justify-content-center">
+        {{ $posts->links() }}
+    </div>
+</div>
 @endsection
